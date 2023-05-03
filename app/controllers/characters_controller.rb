f3769ff512d162bacd1b3aee640af579ca2183d4
andres ibanez <andresibanezulloa@gmail.com>
2023-05-02 22:42:48 -0400

    actualizado

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,43 @@
+class CharactersController < ApplicationController
+  require "rest-client"
+  require "json"
+
+  def index
+    #endpoint: http://localhost:3001/api/v1/characters
+    @result = RestClient.get("http://localhost:3001/api/v1/characters")  
+    @characters = JSON.parse(@result)
+  end
+
+  def show
+    result = RestClient.get("http://localhost:3001/api/v1/characters/#{params[:id]}")
+    @character = JSON.parse(result)      
+    raise
+  end
+
+
+  def new
+    
+  end
+
+  def create
+    result = RestClient.post("http://localhost:3001/api/v1/characters", { 'name' => params["name"], 'actor' => params["actor"] }.to_json, {content_type: :json, accept: :json} )
+
+    if result.code == 201
+      redirect_to characters_path #esto es lo mismo que "/characters"
+    else
+      render "/characters/new", status: :unprocessable_entity
+    end
+   
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+
